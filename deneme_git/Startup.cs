444b2986d4592c70021747e899a454dbbf32951c
tuncdogukan <tuncdogukan@gmail.com>
2016-04-04 19:56:58 +0300

    not

---
+++ deneme_git/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(deneme_git.Startup))]
+namespace deneme_git
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
