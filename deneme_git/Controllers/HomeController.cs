8ebdf6ac5221044bbbac8a0f86b80ac7ca4900a3
krykoc <krykoc@hotmail.com>
2016-04-11 15:19:45 +0300

    not kaydetme eklendi

--- deneme_git/Controllers/HomeController.cs
+++ deneme_git/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
-﻿using CloudNoteV1.Models;
+﻿using Amazon.DynamoDBv2.Model;
+using CloudNoteV1.AWS.DynamoDb;
+using CloudNoteV1.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,7 +30,28 @@ namespace CloudNoteV1.Controllers
 
             return View();
         }
-
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult SaveNote(HomeViewModel model)
+        {
+            DynamoService ds;
+            
+            ds = new DynamoService();
+            string msgContent = Regex.Replace(model.Content, "<.*?>", string.Empty);
+            string msgTitle = Regex.Replace(model.Title, "<.*?>", string.Empty);
+            DateTime now = DateTime.Now;
+            long dateInMilliseconds = (long)(DateTime.Now - new DateTime(1970, 1, 1)).TotalMilliseconds;
+            Dictionary<string, Amazon.DynamoDBv2.Model.AttributeValue> dc = new Dictionary<string, AttributeValue>();
+            dc.Add("item_id", new AttributeValue(dateInMilliseconds.ToString()));
+            dc.Add("Title", new AttributeValue(msgContent));
+            dc.Add("Content", new AttributeValue(msgTitle));
+            dc.Add("Username", new AttributeValue("default"));
+            ds.DynamoClient.PutItem("CloudNoteDb", dc);
+          
+          
+            return View(model);
+        }
+        [ValidateInput(false)]
         public ActionResult CreateNote(HomeViewModel model)
         {
             return View(model);
